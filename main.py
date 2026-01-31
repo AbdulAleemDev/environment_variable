67c192abbdcd9ed22f19f88e0dac532c9936959c
Abdul Aleem <aleemman1234@gmail.com>
2026-01-31 18:41:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from google import genai
+import os
 
+client = genai.Client(api_key=os.environ["Gemini_api"])
+
+response = client.models.generate_content(
+    model="models/gemini-2.5-flash",
+    contents="Explain computer in one sentence"
+)
+
+print(response.text)
